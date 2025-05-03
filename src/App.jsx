6320122b720d84eb8c84eb357a23fa86e7d7e350
logesh <logeshgopi.madasamy@gmail.com>
2025-05-03 12:21:10 +0530

    feat:finsih Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import Header from './components/Header'
+import Header from './components/Header';
+import {Route, Routes, useLocation} from "react-router-dom";
+import Home from './Pages/Home';
 
 const App = () => {
+
+  const isAdminPath = useLocation().pathname.includes("/admin");
   return (
     <div>
-      <Header/>
+      { !isAdminPath &&<Header/>}
+
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+
+      </div>
     </div>
   )
 }
